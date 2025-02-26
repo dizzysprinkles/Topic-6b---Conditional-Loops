406a293a6b7a8ec09b1aa1116b9c1517ca11f679
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-26 13:44:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_6b___Conditional_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
